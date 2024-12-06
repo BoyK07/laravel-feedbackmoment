2e319340bc22dd19810a87c591f0c5839e3782eb
boy5 <boy@modus-digital.com>
2024-12-06 11:15:03 +0100

    Changed seeder

--- my-project/database/seeders/DatabaseSeeder.php
+++ my-project/database/seeders/DatabaseSeeder.php
@@ -6,24 +6,39 @@ use App\Models\User;
 use App\Models\Event;
 use App\Models\Attendee;
 use App\Models\Role;
+use App\Models\RoleUser;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    public function run(): void
-    {
-        Event::factory(10)->create();        // Maak 10 evenementen
-        Attendee::factory(50)->create();     // Maak 50 deelnemers
-        User::factory(20)->create();         // Maak 20 gebruikers
-        Role::factory(5)->create();          // Maak 5 rollen
-        \App\Models\RoleUser::factory(20)->create();     // Maak 20 role_user relaties
+	public function run(): void
+	{
+		Event::factory(10)->create(); // Maak 10 evenementen
+		Attendee::factory(50)->create(); // Maak 50 deelnemers
 
+		// Make an admin and user role
+		Role::factory()->create([
+			'name' => 'admin',
+			'description' => 'Administrator',
+		]);
 
-        // Create admin user
-        User::factory()->create([
-            'name' => 'BoyK07',
-            'email' => 'boy@sadcat.space',
-            'password' => bcrypt('password'),
-        ]);
-    }
+		Role::factory()->create([
+			'name'=> 'user',
+			'description'=> 'User',
+		]);
+
+		// Create admin user
+		User::factory()->create([
+			'name' => 'BoyK07',
+			'email' => 'boy@sadcat.space',
+			'password' => bcrypt('password'),
+		]);
+
+		RoleUser::factory()->create([
+			'user_id' => 1,
+			'role_id' => 1,
+		]);
+
+		User::factory(20)->create(); // Maak 20 gebruikers
+	}
 }
